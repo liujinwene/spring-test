c65753fa7e416d738abe876b50898cda012c3abc
ljw <ljw@qq.com>
2017-07-29 17:54:52 +0800

    init

---
+++ src/main/java/com/test/hello/HelloController.java
@@ -0,0 +1,32 @@
+package com.test.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public User hello(User user) {
+		return user;
+	}
+
+}
+
+class User {
+	private Long id;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
